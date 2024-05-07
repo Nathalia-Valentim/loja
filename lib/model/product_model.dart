5bef3714d0f1ee0c4e4b8da0b6347efb36ad0801
Nathalia-Valentim <126782064+Nathalia-Valentim@users.noreply.github.com>
2024-05-07 18:24:54 -0300

    Delete lib directory

--- lib/model/product_model.dart
+++
@@ -1,9 +0,0 @@
-class ProductModel {
-  String title;
-  String subtitle;
-  String price;
-  String rating;
-  String imageAsset;
-
-  ProductModel({required this.title, required this.subtitle, required this.price, required this.rating, required this.imageAsset});
-}
